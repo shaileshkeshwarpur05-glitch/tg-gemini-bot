c262dc8d91a34e2054b64b738ab91dcbcc03b7a7
shaileshkeshwarpur05-glitch <shaileshkeshwarpur05@gmail.com>
2026-09-13 14:06:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,12 @@ SESSION_STRING = os.environ["SESSION_STRING"]
 GEMINI_KEY = os.environ["GEMINI_API_KEY"]
 
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = # Model initialization
+try:
+    model = genai.GenerativeModel("gemini-1.5-flash-latest")
+except Exception:
+    model = genai.GenerativeModel("gemini-pro")
+    
 
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
